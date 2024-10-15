a4962a1d16f35517060bec80ba5a82d2807295e7
Bertrand Wijaya <bertrandwijaya05@gmail.com>
2024-10-15 15:07:13 +0700

    Add Update and Delete Posts & view Profile

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -6,16 +6,54 @@ import { useRouter } from "next/navigation";
 
 import Profile from "@components/Profile";
 
-const Profile = () => {
+const MyProfile = () => {
+  const router = useRouter();
+    const {data: session} = useSession();
+
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+          const response = await fetch(`/api/users/${session?.user.id}/posts`); // passing dynamic session.
+          const data = await response.json();
+    
+          setPosts(data);
+        }
+    
+        if (session?.user.id) fetchPosts();
+      }, [session?.user.id]);
+
+      
+    const handleEdit = (post) => {
+      router.push(`/update-prompt?id=${post._id}`);
+    };
+
+    const handleDelete = async (post) => {
+      const hasConfirmed = confirm("Are you sure you want to delete this post?");
+
+      if (hasConfirmed) {
+        try {
+          await fetch(`/api/prompt/${post._id.toString()}`, {
+            method: "DELETE",
+          });
+
+          const filteredPosts = posts.filter((p) => p._id !== post._id);
+
+          setPosts(filteredPosts);
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    };
   return (
     <Profile 
         name ="My"
         desc="Welcome to your personalized profile page"
-        data={[]}
-        handleEdit={}
-        handleDelete={}
+        data={posts}
+        handleEdit={handleEdit}
+        handleDelete={handleDelete}
     />
   )
 }
 
-export default Profile;
+export default MyProfile;
